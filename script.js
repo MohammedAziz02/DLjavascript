25b7ac6ec300b712ab203834e2932326d9ea0ebd
Mohammed Aziz <64214635+MohammedAziz02@users.noreply.github.com>
2023-04-27 17:48:54 +0200

    Add functionalities like update the field moyenne on each change (adding or deleting a field)

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ if (telValide(tel1)) {
   console.log("Le numéro de téléphone n'est pas valide");
 }
 
+// function qui calcule le moyenne et mise a jour le champ moyenne
+function calculerMoyenne(){
+  let notes=document.getElementsByName("note[]");
+  console.log(notes);
+  let nombredenotes=notes.length;
+  let somme=0;
+  for(let note of notes  ){
+    somme+=parseInt(note.value);
+  }
+  let moyenne=somme/nombredenotes;
+  console.log(moyenne);
+  document.getElementById("moyenne").value=moyenne;
+}
+
+
 // un variable qui prend en charge que on est sure que a chaque 
 // création d'un autre champs càd l'ajout d'un autre div qui contient
 // les champs label et input sera toujours le cas 
@@ -33,13 +48,6 @@ var compteurDivNote = 2;
 
 //  function pour l'ajout d'un champ de note en cliquant sur la button +
 function ajoutChampNote() {
-
-  //j'ai travailler avec queryselector parceque normalement pour ajouter des divNote qui on le label note est numérote
-  // il faut toujour accéder au dernier divNote pour ajouter un autre divNote après lui pour que l'ordre de label Note sera comme
-  // note 1 --> note 2 --> note 3 ... et non pas note 1 --> note 3 --> note 2  
-  var divs = document.querySelectorAll('div[name="divNote"]');
-  var lastDiv = divs[divs.length - 1];
-
   // pour vérifier que tous les champs sont bien remplis
   let isNotValid = false;
   const inputs = document.getElementsByName("note[]");
@@ -51,43 +59,55 @@ function ajoutChampNote() {
     }
 
   }
-
   if (isNotValid) {
     document.getElementById("adderror").innerText = "veuillez remplir les champs pour ajouter un nouveau champ";
   } else {
-    // Créez un nouveau divNote et ajouter les attributs essentiels
-    // var nouveauDivNote = document.createElement("div");
-    // nouveauDivNote.setAttribute("class", "form-field single-line");
-    // nouveauDivNote.setAttribute("name", "divNote");
-    // Ajoutez les éléments enfants au nouveau divNote 
-    //  Créez un nouveau label et ajouter les attributs essentiels
+    let divnote = document.getElementsByName("divNote")[0];
+    var divaajouter=document.createElement("div");
+    divaajouter.setAttribute("id","divnote"+compteurDivNote);
+    divaajouter.setAttribute("class","input-added")
     var label = document.createElement("label");
     label.setAttribute("for", "note" + compteurDivNote);
     label.setAttribute("name", "note");
     label.textContent = "Note " + compteurDivNote;
-    divs.appendChild(label);
+    divaajouter.append(label);
     // Créez un nouveau input et ajouter les attributs essentiels
     var input = document.createElement("input");
     input.setAttribute("type", "number");
     input.setAttribute("name", "note[]");
     input.setAttribute("id", "note" + compteurDivNote);
-    divs.appendChild(input);
+    input.setAttribute("value",0); 
+    divaajouter.append(input);
     var xdelete = document.createElement("span");
     xdelete.setAttribute("class", "xfordelete");
-    xdelete.setAttribute("id", compteurDivNote);
+    xdelete.setAttribute("id", "x" + compteurDivNote);
     xdelete.textContent = "x";
-    divs.appendChild(xdelete);
-    // Ajoutez le nouveau divNote après le parent
-    // lastDiv.after(nouveauDivNote);
-    // après que on a bien créer le divNote et tous on doit supprimer le text dans le span de l'erreur
+    divaajouter.append(xdelete);
+    divnote.append(divaajouter);
+    calculerMoyenne();
     document.getElementById("adderror").innerText = "";
     compteurDivNote++;
+    
   }
 }
 // l'évenement de la création d'un nouveau div
 document.getElementById("ajouterNote").addEventListener("click", ajoutChampNote);
+// le code qui permet de supprimer les inputs de note que nous avons ajouté en cliquant sur le bouton + et que 
+// nous voulons les supprimer en cliquant sur le bouton x
+document.getElementsByName("divNote")[0].addEventListener("click", function (event) {
+  for (let i = 2; i < 100; i++) {
+    if (event.target.matches("#x"+i)) {
+      document.getElementById("divnote"+i).remove();
+      calculerMoyenne();
+    }
+  }
+});
 
 
+function test(){
+
+  calculerMoyenne();
+}
 
 
 
